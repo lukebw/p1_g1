8b0baa16b3ce2683bb20b4056a0cf3d9a2499eb5
Luke Branam-Wenger <luke.branamwenger@gmail.com>
2026-09-22 18:57:58 -0400

    Setup default input map and implemented basic player movement

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Debug.Log("Hello World!");
+    // Components assigned in Unity Editor
+    public InputAction moveAction;
+    public SpriteRenderer spriteRenderer;
+
+    public Vector2 speed = new Vector2(15, 15);
+    
+    void Start() {
+        moveAction = InputSystem.actions.FindAction("Move");
+        moveAction.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        // Read player directional input, then calculate and apply the change in position
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        Debug.Log("Move Input: " + moveInput.ToString());
+        Vector2 positionDelta = moveInput * speed * Time.deltaTime;
+        transform.position += new Vector3(positionDelta.x, positionDelta.y, 0);
+
+        // Flip sprite horizontally based on movement direction
+        if (moveInput.x < 0) {
+            spriteRenderer.flipX = true;
+        } else if (moveInput.x > 0) {
+            spriteRenderer.flipX = false;
+        }
     }
 }
