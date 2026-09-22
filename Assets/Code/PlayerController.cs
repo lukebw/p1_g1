f08fd3ecec4df7bd4d90c7eca4332812a6e691eb
Luke Branam-Wenger <luke.branamwenger@gmail.com>
2026-09-22 19:19:00 -0400

    Replaced player sprite with transparent version

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -17,7 +17,6 @@ public class PlayerController : MonoBehaviour
     void Update() {
         // Read player directional input, then calculate and apply the change in position
         Vector2 moveInput = moveAction.ReadValue<Vector2>();
-        Debug.Log("Move Input: " + moveInput.ToString());
         Vector2 positionDelta = moveInput * speed * Time.deltaTime;
         transform.position += new Vector3(positionDelta.x, positionDelta.y, 0);
 
